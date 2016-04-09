5129c1ebe6322afd89660a4c207106d03654c392
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-04-09 15:37:01 +0100

    upload sounds when recording is complete

--- app/containers/MarkerOverlay/constants.js
+++ app/containers/MarkerOverlay/constants.js
@@ -10,6 +10,7 @@ export const STOP_AUDIO_RECORDING = 'app/MarkerOverlay/STOP_AUDIO_RECORDING';
 export const AUDIO_RECORDING_STARTED = 'app/MarkerOverlay/AUDIO_RECORDING_STARTED';
 export const AUDIO_RECORDING_COMPLETE = 'app/MarkerOverlay/AUDIO_RECORDING_COMPLETE';
 export const AUDIO_RECORDING_FAILED = 'app/MarkerOverlay/AUDIO_RECORDING_FAILED';
+export const AUDIO_RECORDING_UPLOADED = 'app/MarkerOverlay/AUDIO_RECORDING_UPLOADED';
 export const PLAY_AUDIO = 'app/MarkerOverlay/PLAY_AUDIO';
 export const PAUSE_AUDIO = 'app/MarkerOverlay/PAUSE_AUDIO';
 export const AUDIO_PLAYBACK_COMPLETE = 'app/MarkerOverlay/AUDIO_PLAYBACK_COMPLETE';
