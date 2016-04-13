e747bd4c51e796fdc9616aca8132afb4f196b3d8
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-04-13 18:33:56 +0100

    clean up browser not support page

--- app/components/BrowserNotSupported/index.js
+++ app/components/BrowserNotSupported/index.js
@@ -5,17 +5,18 @@
 */
 
 import React from 'react';
+import OctoCat from 'OctoCat';
 
 function BrowserNotSupported() {
   return (
     <div className="browser">
+      <OctoCat url="https://github.com/thebakeryio/openmic" />
       <div className="branding">
         <a href="/"><h1>openmic</h1></a>
         <div className="logo"></div>
         <h4 className="tagline">Give voice to images.</h4>
       </div>
-      <h3>Sorry but we currently do not support your browser.</h3><h3>You can enjoy Openmic on desktop Chrome (49+) and latest Firefox.</h3>
-      <p>All hate email goes <a href="mailto:hi@thebakery.io">here</a>. Have a nice day!</p>
+      <h3><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder">MediaRecorder API</a> does not work in your browser.</h3><h3>You can try Openmic in your desktop Chrome (49+) and latest Firefox.</h3>
     </div>
   );
 }
